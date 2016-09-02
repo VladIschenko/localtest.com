1fedfa5c76b4dea53129990624cbb1bac303c9ef
Your Name <you@example.com>
2016-09-02 16:07:16 +0300

    added sockets

--- start.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: vlad
- * Date: 31.08.16
- * Time: 13:05
- */
--- test.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>TEST</title>
-</head>
-<body>
-<h1 align="center"><?php echo "HELLO WORLD!" ?></h1>
-</body>
-</html>/
